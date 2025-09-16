4c2b2a2701410387e2510b2d08f6c493f77b5784
AMB-Truong <truong.pham@amoeba.group>
2025-09-16 17:05:58 +0700

    Save status login

--- lib/sign_in_screen.dart
+++ lib/sign_in_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'package:avenue/services/local_storage_service.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
@@ -35,7 +36,8 @@ class _SignInScreenState extends State<SignInScreen> {
             MaterialButton(
               onPressed: () async {
                 final result = await getGoogleAccount();
-                if (result != null) {
+                if (result != null && context.mounted) {
+                  LocalStorageService.saveLoginStatus(true);
                   Navigator.pushReplacementNamed(context, '/home');
                 }
               },
